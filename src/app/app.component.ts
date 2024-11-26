aa44ed1fe2ca712444e94ffffc49e074ab268d76
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-11-26 13:58:51 +0200

    added custom sort pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { DatePipe, DecimalPipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { TemperaturePipe } from './temperature.pipe';
+import { SortPipe } from './sort.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DatePipe, DecimalPipe, TemperaturePipe],
+  imports: [DatePipe, TemperaturePipe, SortPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -26,5 +27,8 @@ export class AppComponent {
 
   onReset(index: number) {
     this.historicTemperatures[index] = 18;
+    //   const newTemps = [...this.historicTemperatures];
+    //   newTemps[index] = 18;
+    //   this.historicTemperatures = newTemps;
   }
 }
